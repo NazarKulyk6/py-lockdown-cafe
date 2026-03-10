45f82914179d4f66d4d37dabe8c848917c2640af
NazarKulyk <156192686+NazarKulyk6@users.noreply.github.com>
2026-03-10 14:41:06 +0100

    Refactor go_to_cafe function and update type hints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError
+from app.errors import (VaccineError, NotWearingMaskError)
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
@@ -13,6 +13,6 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = sum(
         1 for friend in friends if not friend.get("wearing_a_mask")
     )
-    if masks_to_buy > 0:
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
